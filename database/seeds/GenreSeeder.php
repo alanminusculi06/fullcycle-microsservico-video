4a43de85b205a258629f9af84d08e0fd333e8d97
= <=>
2021-03-22 09:53:40 -0300

    excluçsão lógica e relacionamentos

--- database/seeds/GenreSeeder.php
+++ database/seeds/GenreSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use Illuminate\Database\Seeder;
 use App\Models\Genre;
 
@@ -7,6 +8,13 @@ class GenreSeeder extends Seeder
 {
     public function run()
     {
-        factory(Genre::class, 10)->create();
+        // factory(Genre::class, 10)->create();
+        $categories = Category::all();
+        factory(Genre::class, 100)
+            ->create()
+            ->each(function (Genre $genre) use ($categories) {
+                $categoriesId = $categories->random(5)->pluck('id')->toArray();
+                $genre->categories()->attach($categoriesId);
+            });
     }
 }
