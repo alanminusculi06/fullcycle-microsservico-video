98f193b6ae85c0f214874f8da381a7cd0c76529c
Alan Minusculi <alanminusculi@icloud.com>
2021-05-15 14:15:41 -0300

    - aplicado cors no backend; listagem de categorias no frontend

--- frontend/src/pages/category/Table.tsx
+++ frontend/src/pages/category/Table.tsx
@@ -1,9 +1,55 @@
-import { Box } from '@material-ui/core';
 import * as React from 'react';
+import MUIDataTable, { MUIDataTableColumn } from 'mui-datatables';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Chip } from '@material-ui/core';
+import { parseISO, format } from 'date-fns';
+import httpVideo from '../../util/http';
+
+
+const columnsDefinition: MUIDataTableColumn[] = [
+    {
+        name: 'is_active',
+        label: 'Ativo',
+        options: {
+            customBodyRender(value, tableMeta, updateValue) {
+                return value ? <Chip label="Sim" color="primary" /> : <Chip label="Não" color="secondary" />;
+            }
+        }
+    },
+    {
+        name: 'name',
+        label: 'Nome',
+    },
+    {
+        name: 'created_at',
+        label: 'Criado em',
+        options: {
+            customBodyRender(value, tableMeta, updateValue) {
+                return <span>{format(parseISO(value), 'dd/MM/yyyy HH:mm')}</span>;
+            }
+        }
+    },
+]
 
 const Table = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        httpVideo.get('categories').then(
+            response => setData(response.data.data)
+        )
+    }, [])
+
     return (
-        <div>Tabela</div>
+        <MUIDataTable
+            title="Listagem de categorias"
+            columns={columnsDefinition}
+            data={data}
+        >
+
+        </MUIDataTable>
     )
 };
 
